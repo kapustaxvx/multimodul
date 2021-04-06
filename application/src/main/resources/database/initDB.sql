e71dbe08fc90d494cf88236fe069fa8f519b5be4
Ilia Moskalenko <i.moskalenkoxvx@gmail.com>
2021-04-06 12:50:01 +0300

    Task2 create tables: users, tasks

--- application/src/main/resources/database/initDB.sql
+++ application/src/main/resources/database/initDB.sql
@@ -4,3 +4,18 @@ CREATE TABLE IF NOT EXISTS users
     name  VARCHAR(200) NOT NULL ,
     birth_date TIMESTAMP NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS task_statuses
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(200) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS tasks
+(
+    task_id    BIGSERIAL PRIMARY KEY ,
+    status_id BIGINT REFERENCES task_statuses(id),
+    creation_date TIMESTAMP NOT NULL ,
+    title VARCHAR(200) NOT NULL ,
+    expiration_date TIMESTAMP
+);
